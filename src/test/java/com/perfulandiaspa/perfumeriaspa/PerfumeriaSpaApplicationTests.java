f0907cd7b4121fa35a51d1a66c2e93e4f558c66d
BayronAlexisBaezVargas <bay.baez@duocuc.cl>
2025-05-12 13:43:39 -0400

    Primer commit de inicio de proyecto esta vacio solo esta conectado a la base de datos pero no puede hacer mucho ya que no hay codigo dentro de controller/model/repository/service

---
+++ src/test/java/com/perfulandiaspa/perfumeriaspa/PerfumeriaSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandiaspa.perfumeriaspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeriaSpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
